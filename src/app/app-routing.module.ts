d8eb6d6ae9af6b60dc5240099328eaeb7f1c9210
Hugo Gaio Shiroma <hshiroma@acesso.com>
2021-03-22 03:18:45 -0300

    add oficial sector page without photos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HomePage } from './pages/home/home.page';
+import { SectorsPage } from './pages/sectors/sectors.page';
 
 const routes: Routes = [
-  { path: 'home', component: LandingPageComponent },
+  { path: 'home', component: HomePage },
+  { path: 'sectors', component: SectorsPage },
   { path: '**', redirectTo: '/home'}
 ];
 
--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LandingPageComponent } from './landing-page.component';
+import { LandingPageComponent } from './home.page';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
