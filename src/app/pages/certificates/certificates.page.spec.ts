9f0dc1ac239641fc525026c8f991e3970d3c5c7b
Hugo Gaio Shiroma <hshiroma@acesso.com>
2021-06-08 02:58:59 -0300

    faith

--- src/app/pages/certificates/certificates.page.spec.ts
+++ src/app/pages/certificates/certificates.page.spec.ts
@@ -1,10 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-<<<<<<< Updated upstream:src/app/pages/certificates/certificates.component.spec.ts
-import { CertificatesPage } from './certificates.component';
-=======
 import { CertificatesPage } from "./certificates.page";
->>>>>>> Stashed changes:src/app/pages/certificates/certificates.page.spec.ts
 
 describe("CertificatesPage", () => {
   let component: CertificatesPage;
